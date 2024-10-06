77d28ed9ab8087015edfa182a4b47a8ad12a7c40
https://github.com/ElizavetaZhukovaSP/first-project.git <scherbina.lisa.7@mail.ru>
2024-10-06 17:14:09 +0800

    Changed the file index.html

---
+++ hw-2.js
@@ -0,0 +1,54 @@
+//task1
+let a = 10;
+alert(a);
+alert(a = 20);
+
+//task2
+let year= 2007;
+alert(`Год выпуска первого iPhone - ${year}`);
+
+//task3
+let nameCreator = 'Брендан Эйх';
+alert(`${nameCreator} — создатель языка программирования JavaScript`);
+
+//task4
+let b = 10;
+let c = 2;
+alert(b + c);
+alert(b - c);
+alert(b * c);
+alert(b / c);
+
+//task5
+let result = c**5;
+alert(result);
+
+//task6
+let d = 9;
+let remainder = d % c;
+alert(remainder);
+
+//task7
+let num = 1;
+num += 5;//6
+num -= 3;//3
+num *= 7;//21
+num /= 3;//7
+num += 1;//8
+num -= 1;//7
+alert(num);
+
+//task8
+let age = prompt("Сколько Вам лет?");
+alert(age);
+
+//task9
+let user = {
+    name: 'Елизавета',
+    age: '25',
+    isAdmin: true
+};
+
+//task10
+let name = prompt("Как Вас зовут?");
+alert(`Привет, ${name}!`);

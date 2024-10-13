3b7f23dde2ff90a891db538adb9f3ebdb9186cbf
https://github.com/ElizavetaZhukovaSP/first-project.git <scherbina.lisa.7@mail.ru>
2024-10-13 17:48:33 +0800

    Changed the file index.html and make hw-4

--- hw-4.js
+++ hw-4.js
@@ -26,17 +26,55 @@ for (let key in obj) {
 }
 
 //task5
-
+for (let n = 1000, num = 1; n > 50; num++){
+    n /=2;
+    console.log(n);
+    console.log(num);
+}
 //task6
-
+for (let friNumber = 1; friNumber <= 31; friNumber += 7) {
+    if (friNumber >= 1 && friNumber <= 31) {
+        alert(`Сегодня пятница, ${friNumber} -е число. Необходимо подготовить отчет.`)
+    }
+}// в переменной число можно менять от 1 до 7
 
 //Additional tasks 1
-
+for (let k = 100, iterations = 1; k > 0; iterations++){
+    k -= 7;
+    console.log(k);
+    console.log(iterations);
+}
 
 //Additional tasks 2
-
+const months = {
+    1: 'Январь', 2: 'Февраль', 3: 'Март',
+    4: 'Апрель', 5: 'Май', 6: 'Июнь',
+    7: 'Июль', 8: 'Август', 9: 'Сентябрь',
+    10: 'Октябрь', 11: 'Ноябрь', 12: 'Декабрь'   
+}
+let m = Number(prompt('Кокой месяц Вас интресует?'));
+while (1 <= m) {
+    if (m>=1 && m <=12){
+        alert(`Месяц под номером ${m} - это ${months[m]}`);
+        break;
+    }
+    else { 
+        alert('Ввели что-то не то');
+        break;
+    }
+}
+// не получилось реализовать ошибку, если ввести буквы
 
 //Additional tasks 3
+const book = {
+    Название : 'Как устроен JavaScript',
+    Автор : 'Крокфорд Дуглас',
+    "Год издания" : '2019',
+    Жанр : 'Программирование'
+}
+for (let key in book) {
+    console.log(`${key}: ${book[key]}`);
+}
 
 
-//Additional tasks 4
+//Additional tasks 4 :(
